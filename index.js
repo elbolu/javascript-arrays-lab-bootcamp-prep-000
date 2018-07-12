894b9aa5af025e5be64cb1b0521f068151fa4677
Leah Hill <leah@verysandwich.com>
2018-07-12 16:37:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"] 
+
+function destructivelyAppendKitten (name) {
+ kittens.push(name)
+ return kittens
+}
+
+function destructivelyPrependKitten (name){
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten (name) {
+ kittens.pop(name)
+ return kittens
+}
+
+function destructivelyRemoveFirstKitten (name){
+  kittens.shift(name)
+  return kittens
+}
+
+function appendKitten(name) {
+  kittens.concat(name)
+  return kittens.concat(name)
+} 
+
+function prependKitten(name) {
+  kittens.concat(name)
+  return kittens.concat(name)
+} 
+
+fucntion removeLastKitten()
